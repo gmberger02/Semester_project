63b94e22bc31e34dabdf29b74df8e9ef1ba8d1b4
gmberger02 <156141634+gmberger02@users.noreply.github.com>
2024-01-31 14:05:10 +0100

    initial commit

--- modules/user.mjs
+++ modules/user.mjs
@@ -1,16 +1,10 @@
-// denne siden viser hva som trengs for å lage en Single Page Application
 class User {
 
     constructor() {
         ///TODO: Are these the correct fields for your project?
-        this.userID; //unik kode for hver bruker
-        this.email; 
+        this.email;
         this.pswHash;
         this.name;
-        this.username; //egen valg brukernavn
-        this.dateOfBirth; //fødselsdatoen til brukere 
-        this.height; //høyden til brukeren (frivillig)
-        this.weight; //vekten til brukeren (frivillig)
     }
 }
 
--- routes/usersRoute.mjs
+++ routes/usersRoute.mjs
@@ -1,13 +1,22 @@
 import express, { response } from "express";
 import User from "../modules/user.mjs";
-import HttpCodes from "../modules/httpErrorCodes.mjs";
+import { HTTPCodes, HTTPMethods } from "../modules/httpConstants.mjs";
+import SuperLogger from "../modules/SuperLogger.mjs";
+
+
 
 
 const USER_API = express.Router();
+USER_API.use(express.json); // This makes it so that express parses all incoming payloads as JSON for this route.
 
 const users = [];
 
-USER_API.get('/:id', (req, res) => {
+
+USER_API.get('/:id', (req, res, next) => {
+
+
+    SuperLogger.log("Trying to get a user with id " + req.params.id);
+    SuperLogger.log("Bananan is good ");
 
     // Tip: All the information you need to get the id part of the request can be found in the documentation 
     // https://expressjs.com/en/guide/routing.html (Route parameters)
@@ -16,21 +25,16 @@ USER_API.get('/:id', (req, res) => {
     // Return user object
 })
 
-function createUser (req, res, next){
-    const user = new user();
-    user.userId = "w4dhjegyu" //lage unik bruker id
 
-    req.user = user;
-}
-USER_API.post('/', createUser (req, res, next) => {
+USER_API.post('/', (req, res, next) => {
 
     // This is using javascript object destructuring.
     // Recomend reading up https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#syntax
     // https://www.freecodecamp.org/news/javascript-object-destructuring-spread-operator-rest-parameter/
     const { name, email, password } = req.body;
 
-    if (name != "" && email != "" && password != ""){
-        const user = req.user;
+    if (name != "" && email != "" && password != "") {
+        const user = new User();
         user.name = name;
         user.email = email;
 
@@ -42,17 +46,16 @@ USER_API.post('/', createUser (req, res, next) => {
 
         if (!exists) {
             users.push(user);
-            res.status(HttpCodes.SuccesfullRespons.Ok).end();
+            res.status(HTTPCodes.SuccesfullRespons.Ok).end();
         } else {
-            res.status(HttpCodes.ClientSideErrorRespons.BadRequest).end();
+            res.status(HTTPCodes.ClientSideErrorRespons.BadRequest).end();
         }
 
     } else {
-        res.status(HttpCodes.ClientSideErrorRespons.BadRequest).send("Mangler data felt").end();
+        res.status(HTTPCodes.ClientSideErrorRespons.BadRequest).send("Mangler data felt").end();
     }
 
 });
-
 
 USER_API.put('/:id', (req, res) => {
     /// TODO: Edit user
